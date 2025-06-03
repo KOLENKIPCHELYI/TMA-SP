b14c6d9c5111ba4c851b8aeb84b4a5bb4c2c01b1
phela_bzbz <149921073+KOLENKIPCHELYI@users.noreply.github.com>
2025-06-03 17:38:48 +0300

    Update useTasksStore.js

--- src/stores/useTasksStore.js
+++ src/stores/useTasksStore.js
@@ -1,1 +1,11 @@
+import { create } from 'zustand';
 
+const useTasksStore = create((set) => ({
+  tasks: [],
+  currentGroup: null,
+  setTasks: (tasks) => set({ tasks }),
+  addTask: (task) => set((state) => ({ tasks: [...state.tasks, task] })),
+  setCurrentGroup: (group) => set({ currentGroup: group }),
+}));
+
+export default useTasksStore;
