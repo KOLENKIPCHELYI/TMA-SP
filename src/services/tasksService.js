ba0fb6a8f73fe2e42c608f250435b395d39da5f1
phela_bzbz <149921073+KOLENKIPCHELYI@users.noreply.github.com>
2025-06-03 17:39:43 +0300

    Update tasksService.js

--- src/services/tasksService.js
+++ src/services/tasksService.js
@@ -1,1 +1,14 @@
+import { db } from '../firebase/firebaseConfig';
+import { collection, addDoc, query, where, onSnapshot } from "firebase/firestore";
 
+export const addTask = async (groupId, task) => {
+  await addDoc(collection(db, 'groups', groupId, 'tasks'), task);
+};
+
+export const subscribeToTasks = (groupId, callback) => {
+  const q = query(collection(db, 'groups', groupId, 'tasks'));
+  return onSnapshot(q, (snapshot) => {
+    const tasks = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    callback(tasks);
+  });
+};
