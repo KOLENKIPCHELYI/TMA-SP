609797357ed9bc36c6800fa8db73ee25cbd3233c
phela_bzbz <149921073+KOLENKIPCHELYI@users.noreply.github.com>
2025-06-03 17:54:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+exports.generateToken = functions.https.onRequest(async (req, res) => {
+  const { id, username } = req.body;
+
+  // 1. Создаем или обновляем пользователя в Firestore
+  const userRef = admin.firestore().collection('users').doc(String(id));
+  await userRef.set({ username }, { merge: true });
+
+  // 2. Генерируем кастомный токен для Firebase Auth
+  const token = await admin.auth().createCustomToken(String(id));
+
+  res.json({ token });
+});
